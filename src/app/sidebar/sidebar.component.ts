099d9774162fa958c898b469ff2f08e146920ddd
Josue <josue19978@hotmail.com>
2020-02-19 12:30:59 -0500

    Tabla seleccionable

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import {SelectionModel} from '@angular/cdk/collections';
+import {Component} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
     name: string;
-    position: number;
-    weight: number;
-    symbol: string;
+    state: number;
   }
   
-  const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  
+const ELEMENT_DATA: PeriodicElement[] = [
+    {name: 'Hydrogen', state: 1.0079, },
+    {name: 'Helium', state: 4.0026, },
+    {name: 'Lithium', state: 6.941, },
+    {name: 'Beryllium', state: 9.0122,},
+    {name: 'Boron', state: 10.811, },
+    {name: 'Carbon', state: 12.0107, },
+    {name: 'Nitrogen', state: 14.0067, },
+    {name: 'Oxygen', state: 15.9994, },
+    {name: 'Fluorine', state: 18.9984, },
+    { name: 'Neon', state: 20.1797, },
   ];
   
 @Component({
@@ -28,6 +28,29 @@ export interface PeriodicElement {
 })
 
 export class SidebarComponent {
-    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-    dataSource = ELEMENT_DATA;
+    displayedColumns: string[] = ['select', 'name', 'state'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
 }
--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
 import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
-    imports: [ RouterModule, CommonModule, MatTableModule ],
+    imports: [ RouterModule, CommonModule, MatTableModule, MatCheckboxModule ],
     declarations: [ SidebarComponent ],
     exports: [ SidebarComponent ]
 })
