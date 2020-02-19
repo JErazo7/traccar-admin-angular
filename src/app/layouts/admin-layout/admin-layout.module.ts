65502f30c81599717386b6463dca41ec9b7943b1
Josue <josue19978@hotmail.com>
2020-02-19 10:26:29 -0500

    Initial commit

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AdminLayoutRoutes } from './admin-layout.routing';
+
+import { MapsComponent }            from '../../pages/maps/maps.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    NgbModule
+  ],
+  declarations: [
+    MapsComponent,
+  ]
+})
+
+export class AdminLayoutModule {}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { MapsComponent } from '../../pages/maps/maps.component';
+
+export const AdminLayoutRoutes: Routes = [   
+
+    { path: 'maps',           component: MapsComponent },
+    
+];
